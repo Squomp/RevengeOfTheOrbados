cd80c0bc6a55beed90599d7eb01ec5b5f00e4356
Mitchell Hartweg <MHartweg@student.neumont.edu>
2017-02-27 09:09:46 -0700

    adding main method

--- src/edu/neumont/csc150/RevengeOfTheOrbados/RevengeOfTheOrbados.java
+++ src/edu/neumont/csc150/RevengeOfTheOrbados/RevengeOfTheOrbados.java
@@ -1,9 +1,12 @@
 package edu.neumont.csc150.RevengeOfTheOrbados;
 
+import edu.neumont.csc150.RevengeOfTheOrbados.Controller.GameManager;
+
 public class RevengeOfTheOrbados {
 
 	public static void main(String[] args) {
-
+		GameManager gm = new GameManager();
+		gm.run();
 	}
 
 }
