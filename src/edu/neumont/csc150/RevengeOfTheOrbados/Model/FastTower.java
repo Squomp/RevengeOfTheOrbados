72b26f670a0b44aff6105273506286b5f45ba90c
Jeremy Currie <jcurrie@student.neumont.edu>
2017-03-02 14:19:43 -0700

    trying to fix

--- src/edu/neumont/csc150/RevengeOfTheOrbados/Model/FastTower.java
+++ src/edu/neumont/csc150/RevengeOfTheOrbados/Model/FastTower.java
@@ -2,7 +2,8 @@ package edu.neumont.csc150.RevengeOfTheOrbados.Model;
 
 public class FastTower extends Tower{
 	
-	private final int PRICE = 300, DMG = 1, UPGRADE_PRICE = 200, SELL_PRICE = 200, ATTACK_SPEED = 300;
+	private final int PRICE = 300, DMG = 1, UPGRADE_PRICE = 200, SELL_PRICE = 200,
+			ATTACK_SPEED = 300,x=0,y=0;
 	
 	/**
 	 * new default intsance of level 1 FastTower
--- src/edu/neumont/csc150/RevengeOfTheOrbados/Model/LightTower.java
+++ src/edu/neumont/csc150/RevengeOfTheOrbados/Model/LightTower.java
@@ -2,7 +2,8 @@ package edu.neumont.csc150.RevengeOfTheOrbados.Model;
 
 public class LightTower extends Tower{
 
-	private final int PRICE = 250, DMG = 2, UPGRADE_PRICE = 175, SELL_PRICE = 175, ATTACK_SPEED = 800;
+	private final int PRICE = 250, DMG = 2, UPGRADE_PRICE = 175, SELL_PRICE = 175,
+			ATTACK_SPEED = 800,x=0,y=0;
 	
 	/**
 	 * new default instance of level 1 LightTower
